3bf010e466ca9254cf13eabd52efe71107d1f78d
Tanay Tadas <91279967+tanaytadas16@users.noreply.github.com>
2023-07-07 17:44:35 -0500

    added files

---
+++ backend/src/test/java/com/tanay/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanay.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
